92a96905d90810db5cc586b0b7370d59ee44de9d
BelaVoß <bela.voss@B206-21.BK.local>
2021-12-01 09:26:16 +0100

    Klasse mit Zufall

--- DemoProject/src/main/DemoProject.java
+++ DemoProject/src/main/DemoProject.java
@@ -1,10 +1,27 @@
 package main;
 
+import java.util.Random;
+
 public class DemoProject {
 
 	public static void main(String[] args) {
 
-		System.out.println("Huhu");
-
+		
+		Random random = new Random();
+		
+		String wort = "BICYCLE";
+		
+		int zufall;
+		
+		for(int i = 0; i < wort.length(); i++) {
+			
+			zufall = random.nextInt(wort.length());
+				
+			//int[] zufallSpeichern = {zufall}; 
+				
+				System.out.println(wort.charAt(zufall));	
+			
+		}
+		
 	}
 }
